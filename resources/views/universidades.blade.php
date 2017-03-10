52f1e455c8dc645d7d8e8e7371b11a618abd0d6e
Gaby OP <gaby@kokai.com.mx>
2017-03-10 13:00:19 -0600

    Vista Universidades
    Se agrega 1era parte universidades

--- resources/views/universidades.blade.php
+++ resources/views/universidades.blade.php
@@ -1,32 +1,14 @@
 @extends('_master')
 
-<link rel="stylesheet" href="{{URL::asset('/css/empresas.css')}}" type="text/css"> 
+<link rel="stylesheet" href="{{URL::asset('/css/universidades.css')}}" type="text/css"> 
 
 @section ('content')
 	
 	<!--Recursos Economicos-->
 	<div class="flex-container">	
-		<div class="imgRecursos">
-			<div class="titEmpresas">
-				<div class="imgRecursosTxt">
-				</div>
-			</div>	
-		</div>
-	</div>
-	
-	<!--Compromiso Sociedad-->
-	<div class="flex-container">	
-		<div class="imgCompromiso">
-			<div class="imgCompromisoTxt">
-			</div>	
-		</div>
-	</div>
-	
-	<!--Ahorre Tiempo-->
-	<div class="flex-container">	
-		<div class="imgAhorreTiempo">
-			<div class="imgAhorreTiempoTxt">
-			</div>	
+		<div class="imgUniversidades">
+			<div class="imgBeneficiosUniv">
+			</div>
 		</div>
 	</div>
 	
