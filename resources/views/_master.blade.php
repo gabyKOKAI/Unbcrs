2a939635ccb3b3baa30a8d9ab54ad209efcffc41
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2017-03-12 17:57:43 -0600

    Ana es lo maximo prueba2

--- resources/views/_master.blade.php
+++ resources/views/_master.blade.php
@@ -64,7 +64,7 @@
 		</div>
 		
 		<div class="footer">
-			<p>Copyright (c) 2008 Ana es lo maximo </p>
+			<p>Copyright (c) 2008 Ana es lo maximo probando </p>
 		</div>
 		
 			<!-- Bootstrap jQuery -->
