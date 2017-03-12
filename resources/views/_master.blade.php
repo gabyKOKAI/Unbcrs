d961e9d7f3cecc6fa649bcd8b1836d59e41995a0
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2017-03-12 17:58:04 -0600

    Revert "Ana es lo maximo prueba2"
    This reverts commit 2a939635ccb3b3baa30a8d9ab54ad209efcffc41.

--- resources/views/_master.blade.php
+++ resources/views/_master.blade.php
@@ -64,7 +64,7 @@
 		</div>
 		
 		<div class="footer">
-			<p>Copyright (c) 2008 Ana es lo maximo probando </p>
+			<p>Copyright (c) 2008 Ana es lo maximo </p>
 		</div>
 		
 			<!-- Bootstrap jQuery -->
