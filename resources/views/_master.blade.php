1aa2808f85cbeb129c5630e24fcaf48a3a89e364
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2017-03-12 18:01:16 -0600

    prueba2

--- resources/views/_master.blade.php
+++ resources/views/_master.blade.php
@@ -64,7 +64,7 @@
 		</div>
 		
 		<div class="footer">
-			<p>Copyright (c) 2008 Ana es lo maximo probando de nuevo </p>
+			<p>Copyright (c) 2008 Ana es lo maximo probando de nuevo  2</p>
 		</div>
 		
 			<!-- Bootstrap jQuery -->
