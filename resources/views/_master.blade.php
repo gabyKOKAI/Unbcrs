5c989a39986dc0332fdc1882062ab77f2ef646d4
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2017-03-12 18:38:13 -0600

    Footer logo y background

--- resources/views/_master.blade.php
+++ resources/views/_master.blade.php
@@ -64,7 +64,7 @@
 		</div>
 		
 		<div class="footer">
-			<p>Copyright (c) 2008 </p>
+			<img src= "{{URL::asset('/images/logoSoloFooter.png')}}" alt="Unibecarios Facebook"/>
 		</div>
 		
 			<!-- Bootstrap jQuery -->
